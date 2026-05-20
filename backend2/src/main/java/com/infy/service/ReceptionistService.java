3f024a768674ef78dbeef32f768cbf130e01d79f
Krishnendu C J <krishnendu.c.j.2004@gmail.com>
2026-05-20 14:57:46 +0530

    Update ReceptionistService.java

--- backend2/src/main/java/com/infy/service/ReceptionistService.java
+++ backend2/src/main/java/com/infy/service/ReceptionistService.java
@@ -1,61 +1,34 @@
 package com.infy.service;
 
-
- 
-
 import java.util.List;
-
-
- 
-
 import com.infy.dto.AppointmentDTO;
-
 import com.infy.dto.AppointmentSearchDTO;
-
 import com.infy.dto.PatientDTO;
-
 import com.infy.dto.TokenDTO;
-
-
- 
-
+import com.infy.entity.Doctor;
 import com.infy.exception.InfyHospitalException;
 
 
-
- 
-
 public interface ReceptionistService {
 
+    TokenDTO registerWalkIn(PatientDTO PatientDTO, Long doctorId) throws InfyHospitalException;
+    
+    public TokenDTO checkIn(Long appointmentId) throws InfyHospitalException;
+    List<AppointmentSearchDTO> searchPatient(String keyword) throws InfyHospitalException;
 
- 
+    List<TokenDTO> getQueue(Long doctorId) throws InfyHospitalException;
 
-    TokenDTO registerWalkIn(PatientDTO PatientDTO, Long doctorId) throws InfyHospitalException;
-   
-   // Queue position functionality for walk-in registration
-   QueuePositionDTO getQueuePositionAfterWalkIn(Long patientId) throws InfyHospitalException;
+    List<AppointmentDTO> getTodaysAppointment() throws InfyHospitalException;
 
+    void markNoShowAppointments();
 
- 
+    TokenDTO moveToken(Long tokenId, Doctor newDoctor) throws InfyHospitalException;
 
-    public TokenDTO checkIn(Long appointmentId) throws InfyHospitalException;
+    void reorderQueue(Long doctorId, List<Long> tokenIds) throws InfyHospitalException;
 
-    List<AppointmentSearchDTO> searchPatient(String keyword) throws InfyHospitalException;
+    List<TokenDTO> getAllQueues();
+    
+    
 
-
- 
-
-    List<TokenDTO> getQueue(Long doctorId) throws InfyHospitalException;
-
-
- 
-
-    List<AppointmentDTO> getTodaysAppointment() throws InfyHospitalException;
-
-
- 
-
-    void markNoShowAppointments();
-
-   
 }
+ 
