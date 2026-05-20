75571065abfb14d1e41aaef295189b6aac470965
Krishnendu C J <krishnendu.c.j.2004@gmail.com>
2026-05-20 14:44:04 +0530

    Update AuthService.java

--- backend2/src/main/java/com/infy/service/AuthService.java
+++ backend2/src/main/java/com/infy/service/AuthService.java
@@ -1,39 +1,19 @@
 package com.infy.service;
 
-
- 
-
 import com.infy.dto.PatientDTO;
-
 import com.infy.exception.InfyHospitalException;
-
 import com.infy.models.Role;
-
-
- 
 
 public interface AuthService {
 
+    void registerPatient(PatientDTO dto) throws InfyHospitalException;
 
- 
+    void forgotPassword(String email) throws InfyHospitalException;
 
-    void registerPatient(PatientDTO dto) throws InfyHospitalException;
+    void resetPassword(String token, String newPassword) throws InfyHospitalException;
 
+    String login(String email, String password, Role role) throws InfyHospitalException;
 
- 
-
-    String forgotPassword(String email) throws InfyHospitalException;
-
-
- 
-
-    void resetPassword(String token, String newPassword) throws InfyHospitalException;
-
-
- 
-
-    String login(String email, String password, Role role) throws InfyHospitalException;
-
+    void verifyResetToken(String token) throws InfyHospitalException;
 }
-
-
+ 
