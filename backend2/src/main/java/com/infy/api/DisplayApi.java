16be76207a39799aa187ac69e79f00d4c1fb9ac7
Krishnendu C J <krishnendu.c.j.2004@gmail.com>
2026-05-20 14:37:09 +0530

    Update DisplayApi.java

--- backend2/src/main/java/com/infy/api/DisplayApi.java
+++ backend2/src/main/java/com/infy/api/DisplayApi.java
@@ -1,5 +1,29 @@
 package com.infy.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.infy.dto.NowServingResponseDTO;
+import com.infy.service.DisplayService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/clinicq/display")
+@CrossOrigin(origins = "http://localhost:3000")
+@Tag(name = "Display APIs",description = "Operations related to Now serving")
 public class DisplayApi {
+    
+    @Autowired
+    DisplayService displayService;
+    @GetMapping("/nowserving")
+    public ResponseEntity<NowServingResponseDTO> getNowServing() {
+       return ResponseEntity.ok(displayService.getNowServing());
+    }
 
 }
+ 
